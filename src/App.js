c2b205200a17336a5816faae2f665cc1cf5ce3d8
akash rattan <akashrattan43@gmail.com>
2020-06-22 18:01:39 -0700

    halfway

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <nav>
+      <span>Home</span>
+      </nav>
+      <div className="card">
+        <div className="numbers">
+          <div>1</div>
+          <div>/25</div>
+        </div>
+        <div>
+          <data />
+        </div>
+      </div>
+    <footer>
+      <div className="previous">Previous</div>
+      <div className="buttons">
+        <button>Edit</button>
+        <button>Delete</button>
+        <button>New</button>
+      </div>
+      <div className="next">Next</div>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import data from './component/data'
+
+export default class card extends React.Component{
+    constructor(){
+    super()
+    this.state = {
+        data
+        }
+    }
+    
+    
+    render(){
+        return 
+
+}
